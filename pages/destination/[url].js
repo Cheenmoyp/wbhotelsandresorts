6726dd9705dd2e29d06e153170085a7e973f932d
Cheenmoyp <cheenmoy@dzinepixel.com>
2021-12-24 19:38:29 +0530

    Text changed

--- pages/destination/[url].js
+++ pages/destination/[url].js
@@ -212,7 +212,7 @@ const Destination = (response) => {
                                                 </div>
                                                 <div className="amenities">
                                                 <ul>
-                                                    <li><span><img src="/Images/packages/icons/Safe.png" alt="" title=""/></span>ROMT Safe</li>
+                                                    <li><span><img src="/Images/packages/icons/Safe.png" alt="" title=""/></span>WB Safe</li>
                                                     <li><span><img src="/Images/packages/icons/free-wifi.png" alt="" title=""/></span>Free WIFI</li>
                                                     <li><span><img src="/Images/packages/icons/Housekeeping.png" alt="" title=""/></span>Hosekeeping</li>
                                                     <li><a href="#">+More</a></li>
